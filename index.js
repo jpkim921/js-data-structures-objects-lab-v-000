f4fdb54938c5119147cfaf087461c4b947965af5
Jayon Kim <jpkim921@gmail.com>
2018-09-28 18:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const driver = {};
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  return Object.assign({}, driver, { [key] = value} );
-    return Object.assign({}, obj, { [key]: value });
+  return Object.assign({}, driver, { [key]: value} );
+    // return Object.assign({}, obj, { [key]: value });
 
 }
