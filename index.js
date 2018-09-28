3e39bff0e4e692db14b28252c7e257f1faa1ecce
Jayon Kim <jpkim921@gmail.com>
2018-09-28 18:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
 function deleteFromDriverByKey(obj, key) {
   const newObj = Object.assign({}, obj);
   
-  delete newObj.key;
+  delete newObj[key];
 }
